cfe70e162cbaeab011e4a661d7b73f774ef958d1
MINJOO-KIM <64532143+MINJOO-KIM@users.noreply.github.com>
2025-04-10 00:14:00 +0900

    [Silver III] Title: N과 M (3), Time: 2628 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/15651. N과 M （3）/N과 M （3）.py
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.py
@@ -1,29 +1,19 @@
-# 순열
-
-n,m = map(int, input().split())
-
-# 순열로 만들 숫자들을 arr에 넣기
-arr = []
+n, l = map(int, input().split())
+nums = []
 for i in range(1,n+1):
-    arr.append(i)
-# 순열로 넣을 빈 배열 만들기
-path = ['']*m
-used = [0]*n
+    nums.append(i)
+path=['']*l
 
 def dfs(level):
-
-    if level==m:
+    if level==l:
         for i in range(level):
             print(path[i],end=' ')
         print()
         return
-
     for i in range(n):
-        # level 돌 때마다, 숫자 채워주기
-        path[level]=arr[i]
-        # 순열이니까...
-        used[i]=1
-        dfs(level+1)
-        used[i]=0
-dfs(0)
 
+        path[level]=nums[i]
+
+        dfs(level+1)
+
+dfs(0)
