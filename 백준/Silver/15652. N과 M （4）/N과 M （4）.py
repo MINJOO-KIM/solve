4d70e89bb8d59aa4ef63a943ff22aa766ee63b2b
MINJOO-KIM <64532143+MINJOO-KIM@users.noreply.github.com>
2025-04-10 00:18:45 +0900

    [Silver III] Title: N과 M (4), Time: 60 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/15652. N과 M （4）/N과 M （4）.py
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.py
@@ -1,24 +1,18 @@
-n,m = map(int, input().split())
-# n까지 m개씩
-# 중복조합
-ans=[]
-path=['']*m
 
-# ans에서 하나씩
+n, l = map(int, input().split())
+nums = []
 for i in range(1,n+1):
-    ans.append(i)
+    nums.append(i)
+path=['']*l
 
 def dfs(level,start):
-    if level==m:
+    if level==l:
         for i in range(level):
             print(path[i],end=' ')
         print()
         return
-
-    # 중복조합이니까 ans에서 하나씩 뽑고 path에 넣으면 지나가
-        # start 그대로
     for i in range(start,n):
-        path[level]=ans[i]
+        path[level]=nums[i]
         dfs(level+1,i)
 
 dfs(0,0)
