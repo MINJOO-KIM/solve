a551965f90d241819fe622ca0806df66a78682e1
MINJOO-KIM <64532143+MINJOO-KIM@users.noreply.github.com>
2025-06-06 00:52:31 +0900

    [Gold V] Title: 경쟁적 전염, Time: 296 ms, Memory: 117048 KB -BaekjoonHub

--- 백준/Gold/18405. 경쟁적 전염/경쟁적 전염.py
+++ 백준/Gold/18405. 경쟁적 전염/경쟁적 전염.py
@@ -1,44 +1,35 @@
 from collections import deque
 
-n, k = map(int, input().split())
+N, K = map(int, input().split())
+graph = [list(map(int, input().split())) for _ in range(N)]
+visited = [[0] * N for _ in range(N)]
+S, X, Y = map(int, input().split())
 
-graph = [] # 전체 보드 정보를 담는 리스트
-data = [] # 바이러스에 대한 정보를 담는 리스트
+q = deque()
+for virus in range(1, K + 1):
+    for i in range(N):
+        for j in range(N):
+            if graph[i][j] == virus:
+                q.append((virus, 0, i, j))
+                visited[i][j] = 1
 
-for i in range(n):
-    # 보드 정보를 한 줄 단위로 입력
-    graph.append(list(map(int, input().split())))
-    for j in range(n):
-        # 해당 위치에 바이러스가 존재하는 경우
-        if graph[i][j] != 0:
-            # (바이러스 종류, 시간, 위치 X, 위치 Y) 삽입
-            data.append((graph[i][j], 0, i, j))
+dy = [-1, 0, 0, 1]
+dx = [0, -1, 1, 0]
 
-# 정렬 이후에 큐로 옮기기 (낮은 번호의 바이러스가 먼저 증식하므로)
-data.sort()
-q = deque(data)
- 
-target_s, target_x, target_y = map(int, input().split())
- 
-# 바이러스가 퍼져나갈 수 있는 4가지의 위치
-dx = [-1, 0, 1, 0]
-dy = [0, 1, 0, -1]
-
-# 너비 우선 탐색(BFS) 진행
 while q:
-    virus, s, x, y = q.popleft()
-    # 정확히 s초가 지나거나, 큐가 빌 때까지 반복
-    if s == target_s:
+    virus, day, y, x = q.popleft()
+    if day == S:
+        # S초가 되었으면 더 이상 퍼뜨리지 않음
         break
-    # 현재 노드에서 주변 4가지 위치를 각각 확인
-    for i in range(4):
-        nx = x + dx[i]
-        ny = y + dy[i]
-        # 해당 위치로 이동할 수 있는 경우
-        if 0 <= nx and nx < n and 0 <= ny and ny < n:
-            # 아직 방문하지 않은 위치라면, 그 위치에 바이러스 넣기
-            if graph[nx][ny] == 0:
-                graph[nx][ny] = virus
-                q.append((virus, s + 1, nx, ny))
 
-print(graph[target_x - 1][target_y - 1])
+    for i in range(4):
+        ny = y + dy[i]
+        nx = x + dx[i]
+        if 0 <= ny < N and 0 <= nx < N:
+            if visited[ny][nx] == 0 and graph[ny][nx] == 0:
+                graph[ny][nx] = virus
+                visited[ny][nx] = 1
+                q.append((virus, day + 1, ny, nx))
+
+# BFS 종료 후에 결과 출력
+print(graph[X - 1][Y - 1])
