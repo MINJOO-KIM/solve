549d88f24bf5869e6df3ddc5d47ff4d4f39b0d1c
MINJOO-KIM <64532143+MINJOO-KIM@users.noreply.github.com>
2025-06-30 03:19:30 +0900

    [Silver III] Title: 퇴사, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.py
+++ 백준/Silver/14501. 퇴사/퇴사.py
@@ -1,21 +1,17 @@
-def dfs(n, sm):
-    global ans
-    # [1] 종료조건: 가능한 n을 종료에 관련된것으로 정의!
-    if n>=N:
-        ans = max(ans, sm)
-        return
-
-    # [2] 하부호출: 화살표 개수만큼 호출
-    if n+T[n]<=N:   # 상담하는 경우(퇴사일전 완료 가능할 때만 상답)
-        dfs(n+T[n], sm+P[n])
-    dfs(n+1, sm)    # 상담 하지 않는 경우(항상 가능)
-
-N = int(input())
-T = [0]*N
-P = [0]*N
-for i in range(N):
-    T[i], P[i] = map(int, input().split())
-
-ans = 0
-dfs(0, 0)
-print(ans)
+n = int(input())
+t = []
+p = []
+dp = [0]*(n+1)
+for _ in range(n):
+    x,y =map(int, input().split())
+    t.append(x)
+    p.append(y)
+max_result = 0
+for i in range(n-1,-1,-1):
+    time = i + t[i]
+    if time<=n:
+        dp[i]=max(max_result,dp[time]+p[i])
+        max_result=dp[i]
+    else:
+        dp[i]=max_result
+print(max_result)
