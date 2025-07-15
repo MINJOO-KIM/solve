e42be56ee134c54502d93fb0f70551e85dbba2a3
MINJOO-KIM <64532143+MINJOO-KIM@users.noreply.github.com>
2025-07-15 21:04:50 +0900

    [Silver V] Title: 집합, Time: 4132 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/11723. 집합/집합.py
+++ 백준/Silver/11723. 집합/집합.py
@@ -1,25 +1,35 @@
 import sys
 input = sys.stdin.readline
-
-arr = set()
+arr = []
 
 for _ in range(int(input())):
-    parts = input().split()
-    op = parts[0]
-    num = int(parts[1]) if len(parts) > 1 else None
-
-    if op == 'add':
-        arr.add(num)
-    elif op == 'check':
-        print(1 if num in arr else 0)
-    elif op == 'remove':
-        arr.discard(num)
-    elif op == 'toggle':
+    operation_input = list(input().split())
+    operation = operation_input[0]
+    if len(operation_input)>1:
+        num = int(operation_input[1])
+    else:
+        num = None
+    if operation=='add':
+        if num in arr:
+            pass
+        else:
+            arr.append(num)
+    elif operation=='check':
+        if num in arr:
+            print(1)
+        else:
+            print(0)
+    elif operation=='remove':
         if num in arr:
             arr.remove(num)
         else:
-            arr.add(num)
-    elif op == 'all':
-        arr = set(range(1, 21))
-    elif op == 'empty':
-        arr = set()
+            pass
+    elif operation=='toggle':
+        if num in arr:
+            arr.remove(num)
+        else:
+            arr.append(num)
+    elif operation=='all':
+        arr = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+    elif operation=='empty':
+        arr = []
