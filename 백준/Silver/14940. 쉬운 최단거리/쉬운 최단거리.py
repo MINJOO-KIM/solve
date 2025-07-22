4bca8aac6a841665a627557e38e59132c7c98b96
MINJOO-KIM <64532143+MINJOO-KIM@users.noreply.github.com>
2025-07-22 15:55:56 +0900

    [Silver I] Title: 쉬운 최단거리, Time: 748 ms, Memory: 41696 KB -BaekjoonHub

--- 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.py
+++ 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.py
@@ -1,21 +1,13 @@
 import sys
 from collections import deque
-import copy
-
 input = sys.stdin.readline
-INF = int(1e9)
 
 n,m = map(int, input().split())
-graph = []
-visited = [[False]*m for _ in range(n)]
-distance = [[INF]*m for _ in range(n)]
+graph = [list(map(int, input().split())) for _ in range(n)]
+# distance가 -1이 아니면 방문한거임!
+distance = [[-1]*m for _ in range(n)]
 
-for _ in range(n):
-    arr = list(map(int, input().split()))
-    graph.append(arr)
-
-
-gy,gx = 0,0
+gy = gx = -1
 for i in range(n):
     for j in range(m):
         if graph[i][j]==2:
@@ -25,31 +17,22 @@ for i in range(n):
             distance[i][j]=0
 
 # 목표지점부터 시작하기
-q = deque([[gy, gx, 0]]) # 행, 열, 최단거리
-
+q = deque([(gy, gx)]) # 행, 열
 dy = [-1,0,0,1]
 dx = [0,-1,1,0]
 
 while q:
-    y,x,dist=q.popleft()
+    y,x=q.popleft()
     for i in range(4):
         ny = y+dy[i]
         nx = x+dx[i]
         # 배열 범위 체크
-        if ny<0 or nx<0 or ny>=n or nx>=m:
-            continue
-        # 탐색 가능 조건 (아직 방문X, 갈 수 있는 땅)
-            # 최단 거리 갱신
-        if not visited[ny][nx] and graph[ny][nx]==1:
-            visited[ny][nx]=True
-            distance[ny][nx]= min(distance[ny][nx],dist+1)
-            q.append([ny,nx,distance[ny][nx]])
+        if 0 <= ny < n and 0 <= nx < m:
+            # 탐색 가능 조건 (갈 수 있는 땅 and 아직 방문X)
+                # 최단 거리 갱신 (어차피 BFS로 방문하면 그 칸에 도달할 수 있는 가장 빠른 거리)
+            if graph[ny][nx] == 1 and distance[ny][nx] == -1:
+                distance[ny][nx] = distance[y][x] + 1
+                q.append((ny, nx))
 
-# 원래 갈 수 있는데 아예 도달하지 못한 곳 -1 넣기
-for i in range(n):
-    for j in range(m):
-        if not visited[i][j] and graph[i][j]==1:
-            distance[i][j]=-1
-
-for a in range(n):
-    print(*distance[a])
+for row in distance:
+    print(*row)
