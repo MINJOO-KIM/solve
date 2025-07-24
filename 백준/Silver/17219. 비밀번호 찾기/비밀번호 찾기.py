b6da70492a4ab8f28539cd4ba1f27eb4d66c3a78
MINJOO-KIM <64532143+MINJOO-KIM@users.noreply.github.com>
2025-07-25 00:01:48 +0900

    [Silver IV] Title: 비밀번호 찾기, Time: 248 ms, Memory: 48452 KB -BaekjoonHub

---
+++ 백준/Silver/17219. 비밀번호 찾기/비밀번호 찾기.py
@@ -0,0 +1,12 @@
+import sys
+input = sys.stdin.readline
+n,m = map(int, input().split())
+
+dict = {}
+for _ in range(n):
+    site, password = map(str, input().split())
+    dict[site] = password
+
+for _ in range(m):
+    quiz = input().rstrip()
+    print(dict[quiz])
