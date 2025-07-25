31db73b0eb83e50441f0f7d4c1f8f58e0da3ec3e
MINJOO-KIM <64532143+MINJOO-KIM@users.noreply.github.com>
2025-07-25 23:30:32 +0900

    [Silver III] Title: 패션왕 신해빈, Time: 44 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.py
@@ -0,0 +1,21 @@
+tc = int(input())
+for _ in range(1,tc+1):
+    n = int(input())
+    categories = []
+    # [headgear, eyewear]
+    d = {}
+    # {'headgear': ['hat', 'turban'], 'eyewear': ['sunglasses']}
+    for _ in range(n):
+        name, category = map(str, input().split())
+        if category not in categories:
+            categories.append(category)
+            d[category] = []
+            d[category].append(name)
+        else:
+            d[category].append(name)
+    # 경우의 수 구하기
+    result = 1
+    for category in d:
+        result *= (len(d[category])+1)
+    result-=1
+    print(result)
