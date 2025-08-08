ab130916148a943532ba139e586ffd3156121745
MINJOO-KIM <64532143+MINJOO-KIM@users.noreply.github.com>
2025-08-08 23:39:23 +0900

    [Silver I] Title: IOIOI, Time: 328 ms, Memory: 33388 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.py
+++ 백준/Silver/5525. IOIOI/IOIOI.py
@@ -5,11 +5,15 @@ n = int(input())
 m = int(input())
 s = input()
 
-pn = 'IO'*(n)+'I'
+start,end,cnt = 0,0,0
 
-answer = 0
-for i in range(len(s)):
-    if s[i]=='I':
-        if s[i:i+len(pn)]==pn:
-            answer+=1
-print(answer)
+while end < m:
+    if s[end:end+3] == "IOI":
+        end += 2
+        if end - start == 2*n:
+            cnt +=1
+            start +=2
+    else:
+        start = end + 1
+        end = end +1
+print(cnt)
