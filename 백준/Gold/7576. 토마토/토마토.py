d8bef064a696dc6af4554c185db8e4c798086c4a
MINJOO-KIM <64532143+MINJOO-KIM@users.noreply.github.com>
2025-08-20 17:21:11 +0900

    [Gold V] Title: 토마토, Time: 2572 ms, Memory: 108368 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.py
+++ 백준/Gold/7576. 토마토/토마토.py
@@ -1,37 +1,38 @@
+import sys
 from collections import deque
-M,N = map(int, input().split())
-arr = [list(map(int, input().split()))for _ in range(N)]
-used = [[0]*M for _ in range(N)]
+m,n = map(int, input().split())
+lst = [list(map(int, input().split())) for _ in range(n)]
+visited = [[False]*m for _ in range(n)]
 
 q = deque()
-for i in range(N):
-    for j in range(M):
-        if arr[i][j]==1:
-            q.append([i,j,0])
+
+for i in range(n):
+    for j in range(m):
+        if lst[i][j]==1 and not visited[i][j]:
+            q.append((i,j,0))
+            visited[i][j]=True
+
+dy = [-1,0,0,1]
+dx = [0,-1,1,0]
+ans = 0
 
 while q:
     y,x,cnt = q.popleft()
-    directy = [-1,0,0,1]
-    directx = [0,-1,1,0]
-
+    ans = max(ans,cnt)
     for i in range(4):
-        dy = y + directy[i]
-        dx = x + directx[i]
-
-        if dy<0 or dx<0 or dy>=N or dx>=M: continue
-        if arr[dy][dx]==-1: continue
-        if used[dy][dx]==1: continue
-        if arr[dy][dx]==0:
-            arr[dy][dx]=1
-            used[dy][dx]=1
-            q.append([dy,dx,cnt+1])
-
+        ny = y+dy[i]
+        nx = x+dx[i]
+        if 0<=ny<n and 0<=nx<m and not visited[ny][nx] and lst[ny][nx]==0:
+            visited[ny][nx]=True
+            lst[ny][nx]=1
+            q.append((ny,nx,cnt+1))
 flag=0
-for i in range(N):
-    for j in range(M):
-        if arr[i][j]==0:
+for i in range(n):
+    for j in range(m):
+        if lst[i][j]==0:
             flag=1
+
 if flag:
     print(-1)
 else:
-    print(cnt)
+    print(ans)
