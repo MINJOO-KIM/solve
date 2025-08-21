eef519fd93f3f29658c1306bdf25191c82dec1a1
MINJOO-KIM <64532143+MINJOO-KIM@users.noreply.github.com>
2025-08-21 23:46:59 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 2788 ms, Memory: 46504 KB -BaekjoonHub

--- 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.py
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.py
@@ -3,6 +3,7 @@ lst = []
 for _ in range(n):
     x,y = map(int, input().split())
     lst.append((x,y))
-lst.sort(key=lambda x: (x[0],x[1]))
+# lst.sort(key=lambda x: (x[0],x[1]))
+lst.sort()
 for i in range(n):
     print(lst[i][0],lst[i][1])
