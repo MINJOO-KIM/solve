7f9a61af35a1948d8deababf833dcdfb02631a31
MINJOO-KIM <64532143+MINJOO-KIM@users.noreply.github.com>
2025-08-23 13:24:09 +0900

    [Gold V] Title: 적록색약, Time: 68 ms, Memory: 35044 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.py
+++ 백준/Gold/10026. 적록색약/적록색약.py
@@ -1,43 +1,53 @@
+import sys
 from collections import deque
+input = sys.stdin.readline
 
 n = int(input())
-arr = [list(input()) for _ in range(n)]
-visited = [[0]*n for _ in range(n)]
-def BFS(i,j):
+lst = [list(input()) for _ in range(n)]
+
+visited = [[False]*n for _ in range(n)]
+visited_rg = [[False]*n for _ in range(n)]
+
+dy = [-1,0,0,1]
+dx = [0,-1,1,0]
+def bfs_rg(y,x):
     q = deque()
-    q.append([i,j])
-    visited[i][j]=1
+    q.append((y, x))
+
+    while q:
+        y, x = q.popleft()
+        for i in range(4):
+            ny = y + dy[i]
+            nx = x + dx[i]
+            if 0 <= ny < n and 0 <= nx < n and not visited_rg[ny][nx]:
+                if lst[y][x] == 'B' and lst[ny][nx] == 'B':
+                    visited_rg[ny][nx] = True
+                    q.append((ny, nx))
+                if lst[ny][nx] != 'B' and lst[y][x]!='B':
+                    visited_rg[ny][nx] = True
+                    q.append((ny, nx))
+def bfs(y,x):
+    q = deque()
+    q.append((y,x))
 
     while q:
         y,x = q.popleft()
-        directy = [-1,0,0,1]
-        directx = [0,-1,1,0]
         for i in range(4):
-            dy = directy[i]+y
-            dx = directx[i]+x
-            if dy<0 or dx<0 or dy>=n or dx>=n: continue
-            if visited[dy][dx]==1: continue
-            if arr[dy][dx]==arr[y][x]:
-                q.append([dy,dx])
-                visited[dy][dx]=1
-cnt=0
+            ny = y+dy[i]
+            nx = x+dx[i]
+            if 0<=ny<n and 0<=nx<n and not visited[ny][nx]:
+                    if lst[y][x]==lst[ny][nx]:
+                        visited[ny][nx]=True
+                        q.append((ny,nx))
+
+cnt_rg,cnt=0,0
 for i in range(n):
     for j in range(n):
-        if visited[i][j]!=1:
-            BFS(i,j)
+        if not visited_rg[i][j]:
+            bfs_rg(i,j)
+            cnt_rg+=1
+        if not visited[i][j]:
+            bfs(i,j)
             cnt+=1
 
-visited = [[0] * n for _ in range(n)]
-# 적록색약 처리 (R -> G로)
-for i in range(n):
-    for j in range(n):
-        if arr[i][j]=='R':
-            arr[i][j]='G'
-
-cnt2=0
-for i in range(n):
-    for j in range(n):
-        if visited[i][j]!=1:
-            BFS(i,j)
-            cnt2+=1
-print(cnt, cnt2)
+print(cnt,cnt_rg)
