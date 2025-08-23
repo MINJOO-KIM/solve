d8c0a7397738cd798e33cc25a5d583c6c6425bf9
MINJOO-KIM <64532143+MINJOO-KIM@users.noreply.github.com>
2025-08-23 14:29:42 +0900

    [Gold V] Title: 토마토, Time: 820 ms, Memory: 225820 KB -BaekjoonHub

--- 백준/Gold/7569. 토마토/토마토.py
+++ 백준/Gold/7569. 토마토/토마토.py
@@ -1,61 +1,53 @@
-# bfs
-    # 방향이 dy(N) dx(M) dz(H), 6방향
-    # [(-1,0,0), (1,0,0), (0,1,0), (0,-1,0), (0,0,1), (0,0,-1)]
-
-# 3차원 배열 입력받기
-
-# 이동 조건
-    # 방향배열로 체크해서
-        # 배열범위 체크
-        # 0 있으면 + used[dy,dx,dz]가 0이면 -> q에 추가
-
-# 며칠걸리는지
-    # max_cnt, cnt 비교 (q.popleft()할 때)
-
+import sys
 from collections import deque
+input = sys.stdin.readline
 
-M, N, H = map(int, input().split())
-arr = [[list(map(int, input().split())) for _ in range(N)] for _ in range(H)]
-used = [[[0] * M for _ in range(N)] for _ in range(H)]
+m,n,h = map(int, input().split())
+lst = [[list(map(int, input().split())) for _ in range(n)] for _ in range(h)]
+visited = [[[False]*m for _ in range(n)] for _ in range(h)]
+flag=False
+for k in range(h):
+    for i in range(n):
+        for j in range(m):
+            if lst[k][i][j]==0:
+                flag=True
+dz = [-1,1,0,0,0,0]
+dy = [0,0,-1,1,0,0]
+dx = [0,0,0,0,-1,1]
 q = deque()
 
-dz = [-1, 1, 0, 0, 0, 0]
-dy = [0, 0, -1, 1, 0, 0]
-dx = [0, 0, 0, 0, -1, 1]
-
-for i in range(H):
-    for j in range(N):
-        for k in range(M):
-            if arr[i][j][k] == 1:
-                q.append((i, j, k, 0))
-max_cnt =0
-while q:
-    z, y, x, cnt = q.popleft()
-    max_cnt = max(max_cnt, cnt)
-    for i in range(6):
-        nz = z + dz[i]
-        ny = y + dy[i]
-        nx = x + dx[i]
-        if 0 <= nz < H and 0 <= ny < N and 0 <= nx < M:
-            if arr[nz][ny][nx] == 0 and used[nz][ny][nx] == 0:
-                arr[nz][ny][nx] = 1
-                used[nz][ny][nx] = 1
-                q.append((nz, ny, nx, cnt + 1))
-
-max_days = 0
-flag = False
-for i in range(H):
-    for j in range(N):
-        for k in range(M):
-            if arr[i][j][k] == 0:
-                flag = True
-                break
-        if flag:
-            break
-    if flag:
-        break
-
 if flag:
-    print(-1)
+    for k in range(h):
+        for i in range(n):
+            for j in range(m):
+                if lst[k][i][j]==1:
+                    q.append((k,i,j))
+                    visited[k][i][j] = True
+    while q:
+        z, y, x = q.popleft()
+        for i in range(6):
+            nz, ny, nx = z + dz[i], y + dy[i], x + dx[i]
+            if 0 <= nz < h and 0 <= ny < n and 0 <= nx < m and not visited[nz][ny][nx] and lst[nz][ny][nx]==0:
+                visited[nz][ny][nx] = True
+                lst[nz][ny][nx]=lst[z][y][x]+1
+                q.append((nz, ny, nx))
+    flag_final=False
+    for k in range(h):
+        for i in range(n):
+            for j in range(m):
+                # bfs 수행했지만 토마토가 익지 않았을 때
+                if lst[k][i][j] == 0:
+                    flag_final = True
+    if flag_final:
+        print(-1)
+    # 토마토 모두 익었다면 최댓값 출력하기
+    else:
+        ans=0
+        for k in range(h):
+            for i in range(n):
+                for j in range(m):
+                    ans=max(ans,lst[k][i][j])
+        print(ans-1)
+# 처음부터 모든 토마토가 익어있으면 (0이 없다면)
 else:
-    print(max_cnt)
+    print(0)
