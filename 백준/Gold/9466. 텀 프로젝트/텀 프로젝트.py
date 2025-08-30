0e0120b6b5b12f5ec20d48ba15502485f557d6c6
MINJOO-KIM <64532143+MINJOO-KIM@users.noreply.github.com>
2025-08-30 21:51:20 +0900

    [Gold III] Title: 텀 프로젝트, Time: 2252 ms, Memory: 59392 KB -BaekjoonHub

--- 백준/Gold/9466. 텀 프로젝트/텀 프로젝트.py
+++ 백준/Gold/9466. 텀 프로젝트/텀 프로젝트.py
@@ -1,30 +1,35 @@
 import sys
-input = sys.stdin.readline
 sys.setrecursionlimit(10**6)
+input = sys.stdin.readline
 
-def dfs(n,result):
-    visited[n]=True
-    cycle.append(n)
+def dfs(x):
+    global cnt
+    visited[x] = True
+    next = nums[x]
 
-    next = nums[n]
-    # 방문했으면 사이클에 있는지 확인
-    if  visited[next]:
-        if next in cycle:
-            # 연속적으로 cycle에 추가하기때문에
-            result += cycle[cycle.index(next):]
-    # 아직 방문안했으면 계속 탐색
+    # 방문 안했으면 계속 탐색
+    if not visited[next]:
+        dfs(next)
+    # 방문했으면 사이클 있는지 확인하기
     else:
-        dfs(next,result)
+        if not finished[next]:  # 사이클 발견
+            cur = next
+            cycle_size = 1
+            while cur != x:
+                cur = nums[cur]
+                cycle_size += 1
+            cnt += cycle_size
+
+    finished[x] = True  # 탐색 끝 표시
 
 for _ in range(int(input())):
     n = int(input())
-    nums = [0]+list(map(int, input().split()))
-    visited = [True]+[False]*n
+    nums = [0] + list(map(int, input().split()))
+    visited = [False] * (n+1)
+    finished = [False] * (n+1)
+    cnt = 0
 
-    result = []
-    for i in range(1,n+1):
+    for i in range(1, n+1):
         if not visited[i]:
-            cycle = []
-            dfs(i,result)
-
-    print(n-len(result))
+            dfs(i)
+    print(n - cnt)
