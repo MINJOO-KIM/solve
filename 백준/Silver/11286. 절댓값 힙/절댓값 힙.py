eaf8cade3621c86f0235192ea48f958d7777b76e
MINJOO-KIM <64532143+MINJOO-KIM@users.noreply.github.com>
2025-09-25 01:00:02 +0900

    [Silver I] Title: 절댓값 힙, Time: 128 ms, Memory: 42144 KB -BaekjoonHub

--- 백준/Silver/11286. 절댓값 힙/절댓값 힙.py
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.py
@@ -1,18 +1,16 @@
-import sys
 import heapq
-
+import sys
 input = sys.stdin.readline
-
 n = int(input())
-h = []
-
+heap = []
 for _ in range(n):
-    num = int(input())
-    if num != 0:
-        heapq.heappush(h, (abs(num),num))
-    else:
-        if h:
-            # 절댓값이 가장 작은 기준으로 실제 값
-            print(heapq.heappop(h)[1])
+    x = int(input())
+    if x==0:
+        if heap:
+            # 절댓값이 같은게 여러개일때 가장 작은 수 출력
+            print(heapq.heappop(heap)[1])
         else:
             print(0)
+    else:
+        # 튜플 (a, b) -> a값을 먼저 비교하고 b값을 비교
+        heapq.heappush(heap,(abs(x),x))
