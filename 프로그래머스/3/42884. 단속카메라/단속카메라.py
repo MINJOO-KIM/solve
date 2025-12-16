2fe773c48160476c856f595eaffd31aa48e1132a
MINJOO-KIM <64532143+MINJOO-KIM@users.noreply.github.com>
2025-12-16 17:15:07 +0900

    [level 3] Title: 단속카메라, Time: 1.10 ms, Memory: 9.67 MB -BaekjoonHub

---
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.py
@@ -0,0 +1,13 @@
+def solution(routes):
+    answer = 1
+    
+    routes.sort(key=lambda x: x[1])
+    camera = routes[0][1]
+    
+    for i in range(1,len(routes)):
+        if routes[i][0] <= camera <= routes[i][1]:
+            continue
+        else:
+            answer+=1
+            camera = routes[i][1]
+    return answer
