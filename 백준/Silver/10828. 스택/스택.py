0a77356155824612199ec62bbb15e735fce67c69
MINJOO-KIM <64532143+MINJOO-KIM@users.noreply.github.com>
2026-03-09 12:08:04 +0900

    [Silver IV] Title: 스택, Time: 40 ms, Memory: 33432 KB -BaekjoonHub

---
+++ 백준/Silver/10828. 스택/스택.py
@@ -0,0 +1,24 @@
+import sys
+input = sys.stdin.readline
+s = []
+for _ in range(int(input())):
+    command = input().split()
+    if command[0]=='push':
+        s.append(int(command[1]))
+    elif command[0]=='pop':
+        if s:
+            print(s.pop())
+        else:
+            print(-1)
+    elif command[0]=='size':
+        print(len(s))
+    elif command[0]=='empty':
+        if s:
+            print(0)
+        else:
+            print(1)
+    elif command[0]=='top':
+        if s:
+            print(s[-1])
+        else:
+            print(-1)
