9b87aee30d318beaaf7831f51bf7623e8071060f
MINJOO-KIM <64532143+MINJOO-KIM@users.noreply.github.com>
2026-03-13 19:13:16 +0900

    [Silver II] Title: 스택 수열, Time: 2588 ms, Memory: 38876 KB -BaekjoonHub

---
+++ 백준/Silver/1874. 스택 수열/스택 수열.py
@@ -0,0 +1,30 @@
+n = int(input())
+targets = []
+
+for _ in range(n):
+    targets.append(int(input()))
+s = [] 
+ans = []
+
+current = 1
+flag = 1
+
+for target in targets:
+    while current<=target:
+        s.append(current)
+        current+=1
+        ans.append('+')
+
+    if s[-1] == target:
+        if s:
+            s.pop()
+            ans.append('-')
+    else:
+        flag = 0
+        break
+        
+if flag:
+    for i in range(len(ans)):
+        print(ans[i])
+else:
+    print("NO")
