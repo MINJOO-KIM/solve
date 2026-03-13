5a6ff59610bf5563460b5d0f403ea358075703c7
MINJOO-KIM <64532143+MINJOO-KIM@users.noreply.github.com>
2026-03-13 19:13:46 +0900

    [Silver II] Title: 스택 수열, Time: 196 ms, Memory: 38876 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.py
+++ 백준/Silver/1874. 스택 수열/스택 수열.py
@@ -1,9 +1,11 @@
+import sys
+input = sys.stdin.readline
 n = int(input())
 targets = []
 
 for _ in range(n):
     targets.append(int(input()))
-s = [] 
+s = []
 ans = []
 
 current = 1
@@ -19,10 +21,10 @@ for target in targets:
         if s:
             s.pop()
             ans.append('-')
+
     else:
         flag = 0
         break
-        
 if flag:
     for i in range(len(ans)):
         print(ans[i])
