9866dacd02d2b49d3c36974de58fe48138fabc47
MINJOO-KIM <64532143+MINJOO-KIM@users.noreply.github.com>
2026-03-22 23:31:06 +0900

    [Silver IV] Title: 덱, Time: 68 ms, Memory: 34952 KB -BaekjoonHub

---
+++ 백준/Silver/10866. 덱/덱.py
@@ -0,0 +1,38 @@
+import sys
+input = sys.stdin.readline
+from collections import deque
+
+q = deque()
+for _ in range(int(input())):
+    command = input().split()
+    if command[0]=='push_front':
+        q.appendleft(int(command[1]))
+    elif command[0] == 'push_back':
+        q.append(int(command[1]))
+    elif command[0]== 'pop_front':
+        if q:
+            print(q.popleft())
+        else:
+            print(-1)
+    elif command[0]== 'pop_back':
+        if q:
+            print(q.pop())
+        else:
+            print(-1)
+    elif command[0]=='size':
+        print(len(q))
+    elif command[0]=='empty':
+        if q:
+            print(0)
+        else:
+            print(1)
+    elif command[0]=='front':
+        if q:
+            print(q[0])
+        else:
+            print(-1)
+    elif command[0]=='back':
+        if q:
+            print(q[-1])
+        else:
+            print(-1)
